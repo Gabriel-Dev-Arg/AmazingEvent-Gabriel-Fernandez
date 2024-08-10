4b609229002284e7180ebd62f5fa39ef2ab96f21
gabriel <gabriel199777@outlook.com>
2024-08-10 13:18:21 -0300

    actualizado

--- scripts/details.js
+++ scripts/details.js
@@ -195,89 +195,46 @@ const data = {
     ],
 };
 
-let futureEventsContainer = document.getElementById("future-events-container");
-let check = document.getElementById("checked");
 
-
-let navChecks = document.createElement("div");
-navChecks.className = "bg-blue mb-4 text-white ";
-navChecks.innerHTML = `
-<div class="d-flex col-12 col-sm-12 fs-6 p-4 flex-wrap justify-content-between bg-blue text-white">
-    <div class="d-flex gap-3 justify-content-center flex-row flex-wrap flex-lg-row flex-md-row flex-sm-row col-12 col-sm-12 col-md-8 "id="category-checkboxes">
-    </div>
-    <div class="d-flex col-12 col-lg col-md justify-content-center>
-        <form class="d-flex d-flex col-6 col-lg col-md " role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="searchInput">
-            <button class="btn bg-white" type="button" id="searchButton">
-                <i class="bi bi-search fs-6"></i>
-            </button>
-        </form>
-    </div>
-</div>
-
-`;
-
-check.appendChild(navChecks);
-
-const categories = [...new Set(data.events.map(event => event.category))];
-const checkboxesContainer = document.getElementById("category-checkboxes");
-
-categories.forEach((category, index) => {
-    const checkbox = document.createElement("div");
-    checkbox.className = "form-check form-check-inline";
-    checkbox.innerHTML = `
-        <input class="form-check-input bg-gray" type="checkbox" id="category${index}" value="${category}">
-        <label class="form-check-label" for="category${index}">${category}</label>
-    `;
-    checkboxesContainer.appendChild(checkbox);
-});
-
-function filterEvents() {
-    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
-    const selectedCategories = Array.from(document.querySelectorAll(".form-check-input:checked")).map(checkbox => checkbox.value);
-
-    const filteredEvents = data.events.filter(event => {
-        const matchesSearch = event.name.toLowerCase().includes(searchTerm) || event.description.toLowerCase().includes(searchTerm);
-        const matchesCategory = selectedCategories.length === 0 || selectedCategories.includes(event.category);
-        return event.date >= data.currentDate && matchesSearch && matchesCategory;
-    });
-
-    renderEvents(filteredEvents);
+function getEventById(id) {
+    return data.events.find(event => event._id === id);
 }
 
-function renderEvents(events) {
-    futureEventsContainer.innerHTML = '';
-    if (events.length === 0) {
-        futureEventsContainer.innerHTML = `
-            <div class="col-12 text-center">
-                <h3>Oops! No encontramos eventos que coincidan con tu búsqueda.</h3>
-                <p>Intenta ajustar tus filtros o busca algo diferente.</p>
+function displayEventDetails(event) {
+    const eventDetailsContainer = document.getElementById('event-details');
+    if (event) {
+        eventDetailsContainer.innerHTML = `
+            <div class="align-items-center card m-1 d-flex col-8 col-sm-8 col-lg-8 flex-sm-column flex-md-column flex-lg-row flex-column justify-content-center col-12">
+                <img class="card-imagen col-8 col-lg-6 col-sm-8 col-md-8" src="${event.image}">
+                <div class="card-body col-12 col-lg-6">
+                    <h5 class="card-title text-center">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                    <p><strong>Category:</strong> ${event.category}</p>
+                    <p><strong>Date:</strong> ${event.date}</p>
+                    <p><strong>Place:</strong> ${event.place}</p>
+                    <p><strong>Price:</strong> $${event.price}</p>
+                    <p><strong>Capacity:</strong> ${event.capacity}</p>
+                    ${event.assistance ? `<p><strong>Assistance:</strong> ${event.assistance}</p>` : ''}
+                    ${event.estimate ? `<p><strong>Estimate:</strong> ${event.estimate}</p>` : ''}
+                </div>
             </div>
         `;
     } else {
-        events.forEach(event => {
-            let tarjeta = document.createElement("div");
-            tarjeta.className = "col-md-4 col-9 col-sm-6 col-lg-3";
-            tarjeta.innerHTML = `
-                <div class="card m-1 width_18rem ">
-                    <img class="card-img-top" src="${event.image}">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">${event.name}</h5>
-                        <p class="card-text h-parrafo">${event.description}</p>
-                        <div class="d-flex justify-content-between"><p><strong>Price:</strong> $${event.price}
-                        </p><a href="./Details.html?id=${event._id}" class="btn btn-primary">Details</a>
-                        </div>
-                    </div>
-                </div>
-            `;
-            futureEventsContainer.appendChild(tarjeta);
-        });
+        eventDetailsContainer.innerHTML = '<p>Event not found</p>';
     }
 }
 
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
 
-document.getElementById('searchInput').addEventListener('button', filterEvents);
-document.querySelectorAll(".form-check-input").forEach(checkbox => checkbox.addEventListener('change', filterEvents));
-
-
-filterEvents();
+document.addEventListener('DOMContentLoaded', () => {
+    const eventId = getQueryParam('id');
+    if (eventId) {
+        const event = getEventById(eventId);
+        displayEventDetails(event);
+    } else {
+        document.getElementById('event-details').innerHTML = '<p>No event ID provided</p>';
+    }
+});
--- scripts/pastEvents.js
+++ scripts/pastEvents.js
@@ -195,24 +195,87 @@ const data = {
     ],
 };
 let pastEventsContainer = document.getElementById("past-events-container");
+let check = document.getElementById("checked");
 
-for (let i = 0; i < data.events.length; i++) {
-    let eventDate = data.events[i].date;
-    if (eventDate < data.currentDate) {
-        let tarjeta = document.createElement("div");
-        tarjeta.className = "col-md-3";
 
-        tarjeta.innerHTML = `
-            <div class="card m-1 width_18rem">
-                <img class="card-img-top" src="${data.events[i].image}">
-                <div class="card-body">
-                    <h5 class="card-title text-center">${data.events[i].name}</h5>
-                    <p class="card-text h-parrafo">${data.events[i].description}</p>
-                    <div class="d-flex justify-content-between"><p><strong>Price:</strong> $${data.events[i].price}</p><a href="./Details.html" class="btn btn-primary">Details</a></div>
-                </div>
+let navChecks = document.createElement("div");
+navChecks.className = "bg-blue mb-4 text-white ";
+navChecks.innerHTML = `
+<div class="d-flex col-12 col-sm-12 fs-6 p-4 flex-wrap justify-content-between bg-blue text-white">
+    <div class="d-flex gap-3 justify-content-center flex-row flex-wrap flex-lg-row flex-md-row flex-sm-row col-12 col-sm-12 col-md-8 "id="category-checkboxes">
+    </div>
+    <div class="d-flex col-12 col-lg col-md justify-content-center>
+        <form class="d-flex d-flex col-6 col-lg col-md " role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="searchInput">
+            <button class="btn bg-white" type="button" id="searchButton">
+                <i class="bi bi-search fs-6"></i>
+            </button>
+        </form>
+    </div>
+</div>
+
+`;
+
+check.appendChild(navChecks);
+
+const categories = [...new Set(data.events.map(event => event.category))];
+const checkboxesContainer = document.getElementById("category-checkboxes");
+
+categories.forEach((category, index) => {
+    const checkbox = document.createElement("div");
+    checkbox.className = "form-check form-check-inline";
+    checkbox.innerHTML = `
+        <input class="form-check-input bg-gray" type="checkbox" id="category${index}" value="${category}">
+        <label class="form-check-label" for="category${index}">${category}</label>
+    `;
+    checkboxesContainer.appendChild(checkbox);
+});
+
+function filterEvents() {
+    const searchTerm = document.getElementById('searchInput').value.toLowerCase();
+    const selectedCategories = Array.from(document.querySelectorAll(".form-check-input:checked")).map(checkbox => checkbox.value);
+
+    const filteredEvents = data.events.filter(event => {
+        const matchesSearch = event.name.toLowerCase().includes(searchTerm) || event.description.toLowerCase().includes(searchTerm);
+        const matchesCategory = selectedCategories.length === 0 || selectedCategories.includes(event.category);
+        return event.date >= data.currentDate && matchesSearch && matchesCategory;
+    });
+
+    renderEvents(filteredEvents);
+}
+
+function renderEvents(events) {
+pastEventsContainer.innerHTML = '';
+    if (events.length === 0) {
+        pastEventsContainer.innerHTML = `
+            <div class="col-12 text-center">
+                <h3>Oops! No encontramos eventos que coincidan con tu búsqueda.</h3>
+                <p>Intenta ajustar tus filtros o busca algo diferente.</p>
             </div>
         `;
-
-        pastEventsContainer.appendChild(tarjeta);
+    } else {
+        events.forEach(event => {
+            let tarjeta = document.createElement("div");
+            tarjeta.className = "col-md-4 col-9 col-sm-6 col-lg-3";
+            tarjeta.innerHTML = `
+                <div class="card m-1 width_18rem ">
+                    <img class="card-img-top" src="${event.image}">
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${event.name}</h5>
+                        <p class="card-text h-parrafo">${event.description}</p>
+                        <div class="d-flex justify-content-between"><p><strong>Price:</strong> $${event.price}
+                        </p><a href="./Details.html?id=${event._id}" class="btn btn-primary">Details</a></div>
+                    </div>
+                </div>
+            `;
+            pastEventsContainer.appendChild(tarjeta);
+        });
     }
 }
+
+
+document.getElementById('searchInput').addEventListener('button', filterEvents);
+document.querySelectorAll(".form-check-input").forEach(checkbox => checkbox.addEventListener('change', filterEvents));
+
+
+filterEvents();
